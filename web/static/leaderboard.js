ebab7869824e0cd6e0bda21d4eb751a6b59d82d5
Steffen F. Qvistgaard <steffen@sublife.dk>
2021-10-13 20:43:22 +0200

    feat: updated leaderboard ui

--- web/static/leaderboard.js
+++ web/static/leaderboard.js
@@ -4,10 +4,31 @@ const app = Vue.createApp({
 
     computed: {
         leaderboard: function(){
+            console.log("RELOAD")
+
             return this.$store.getters.getRaceState("race-leaderboard", { entries: [] }).entries
               .map(x =>  {
                   let v = moment.duration(x["lap"]["lap-time"] / 1000 / 1000).asSeconds()
                   x["lap"]["lap-time"] = Number((v)).toFixed(3)
+
+                  let b = moment.duration(x["best"]["lap-time"] / 1000 / 1000).asSeconds()
+                  x["best"]["lap-time"] = Number((b)).toFixed(3)
+
+                  let d = moment.duration(x["delta"] / 1000 / 1000).asSeconds()
+
+                  x["isSlow"] = false
+                  x["isFast"] = false
+
+                  if (d > 0) {
+                      x["isSlow"] = true
+                      prefix = "+"
+                  } else if (d < 0){
+                      x["isFast"] = true
+                      prefix = ""
+                  } else {
+                      prefix = ""
+                  }
+                  x["delta"] = prefix+Number((d)).toFixed(3)
                   return x
               })
         },
